3dc509f180e9eefe16dbc07496e20ec5bc50894e
MoXuToS <moxutosic@gmail.com>
2024-04-23 18:18:48 +0300

    NPC class

---
+++ Assets/_Scripts/NPC.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Mesh;
+
+    /// <summary>
+    /// Отвечает за передвижение персонажей
+    /// </summary>
+    public override void Move(Vector3 direction)
+    {
+
+    }
+
+    /// <summary>
+    /// Отвечает за связь с движениями
+    /// </summary>
+    public override void DestroyNPC()
+    {
+
+    }
+}
